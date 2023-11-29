5939729b89b9901c86b305439f93947c597f486f
shubham bajaj <shmbajaj32@gmail.com>
2023-11-29 21:22:12 +0530

    created questionsviews

--- QuizMaster/View/QuestionsView.swift
+++ QuizMaster/View/QuestionsView.swift
@@ -2,17 +2,86 @@
 //  QuestionsView.swift
 //  QuizMaster
 //
-//  Created by Shubham Bajaj on 29/11/23.
-//
 
 import SwiftUI
 
 struct QuestionsView: View {
+    var quizInfo: Info;
+    var quizQuestions: [Question];
+    @Environment(\.dismiss) private var dismiss
+    @State private var progress: CGFloat = 0
+    @State private var currentIndex: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 15){
+            Button{
+                dismiss()
+            }
+        label:{
+            Image(systemName: "xmark")
+                .font(.title3)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+        }.hAlign(.leading)
+            
+            Text(quizInfo.title).font(.title).fontWeight(.semibold).foregroundColor(.white)
+            
+            GeometryReader{
+                let size = $0.size
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .fill(.black.opacity (0.2))
+                    Rectangle()
+                        .fill(Color("Progress"))
+                        .frame(width: progress * size.width, alignment: .leading)
+                }
+                .clipShape(Capsule())
+            }
+            .frame(height: 20)
+            .padding (.top,5)
+            
+            // - Questions
+            GeometryReader{
+                let size = $0.size
+                
+                ForEach(quizQuestions.indices, id: \.self) { index in
+                    // Using transition for moving forth and between instead of using tabview
+
+                    if currentIndex == index {
+                        // view enters from left and leaves towards right
+                        QuestionView(quizQuestions[currentIndex])
+                            .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+                    }
+                    // Add other views or logic related to each element here
+                }
+
+            }.padding(.vertical, 15)
+            
+            CustomButton(title: "Next Question", onClick: {
+                
+            })
+            
+        }
+        .padding(15)
+        .hAlign(.center).vAlign(.top)
+        .background {
+            Color("BG")
+                .ignoresSafeArea()
+        }
+        /// This View is going to be Dark Since our background is Dark
+        .environment(\.colorScheme, .dark)
     }
+    
+    // Question View
+    @ViewBuilder
+    func QuestionView(_ question: Question)-> some View{
+        RoundedRectangle(cornerRadius: 20, style: .continuous)
+            .fill(.white)
+    }
+    
+    
 }
 
 #Preview {
-    QuestionsView()
+    ContentView()
 }
