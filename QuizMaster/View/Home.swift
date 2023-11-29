f2516fb169502f1e7c464ff7cbfdab0da0d0fc7b
shubham bajaj <shmbajaj32@gmail.com>
2023-11-29 19:40:09 +0530

    created views custom-label and rules-view

--- QuizMaster/View/Home.swift
+++ QuizMaster/View/Home.swift
@@ -11,9 +11,24 @@ struct Home: View {
     @State private var quizInfo: Info?
     @State private var quizQuestions: [Question] = [];
     @AppStorage("log_status") private var logStatus: Bool = false;
+    
     var body: some View {
         if let info = quizInfo {
-            Text(info.title)
+            VStack(spacing: 10){
+                Text(info.title).font(.title).fontWeight(.semibold).hAlign(.leading)
+                /// - Custom Label
+                
+                CustomLabel("list.bullet.rectangle.portrait", "\(quizQuestions.count)", "Multiple Choice Question").padding(.top, 20)
+                
+                CustomLabel("person", "\(info.peopleAttended)", "Attended the exercise").padding(.top, 5)
+                
+                Divider().padding(.horizontal, -15).padding(.top, 15)
+                
+                if(!info.rules.isEmpty){
+                    RulesView(info.rules)
+                }
+                
+            }.padding(15).vAlign(.top)
         }else{
             VStack(spacing: 4){
                 ProgressView()
@@ -30,6 +45,53 @@ struct Home: View {
         }
     }
     
+    // - Rules View
+    @ViewBuilder
+    func RulesView(_ rules: [String]) -> some View{
+        VStack(alignment: .leading, spacing: 15){
+            Text("Before you start")
+                .font(.title3)
+                .fontWeight(.bold)
+                .padding(.bottom, 12)
+            
+            ForEach(rules, id: \.self){rule in
+                HStack(alignment: .top, spacing: 10){
+                    Circle().fill(.black).frame(width: 8, height: 8)
+                    Text(rule).font(.callout).lineLimit(3)
+                }
+            }
+        }
+    }
+    
+    /// - Custom Label
+    @ViewBuilder
+    func CustomLabel(_ image: String, _ title: String, _ subTitle: String) -> some View {
+        HStack(spacing: 12) {
+            Image(systemName: image)
+                .font(.title3)
+                .frame(width: 45, height: 45)
+                .background {
+                    Circle()
+                        .fill(Color.gray.opacity(0.1))
+                        .padding(-1)
+                        .background {
+                            Circle().stroke(Color("BG"), lineWidth: 1)
+                        }
+                }
+
+            VStack(alignment: .leading, spacing: 4) {
+                Text(title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("BG"))
+
+                Text(subTitle)
+                    .font(.caption)
+                    .fontWeight(.medium)
+                    .foregroundColor(.gray)
+            }.hAlign(.leading)
+        }
+    }
+
     func fetchData()async throws{
         try await logInUserAnonymous()
         let info = try await Firestore.firestore().collection("Quiz").document("Info").getDocument().data(as: Info.self)
@@ -54,3 +116,15 @@ struct Home: View {
     Home()
 }
 
+// MARK: View Extensions
+/// - Useful for moving views btw HStack and VStack
+
+extension View{
+    func hAlign(_ alignment: Alignment) -> some View {
+        self.frame(maxWidth: .infinity, alignment: alignment)
+    }
+    
+    func vAlign(_ alignment: Alignment) -> some View {
+        self.frame(maxHeight: .infinity, alignment: alignment)
+    }
+}
