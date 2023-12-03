4de3c3c58ace49e7ed4c8aca16ba64d1940bed2c
shubhambajaj <shmbajaj32@gmail.com>
2023-12-03 23:39:02 +0530

    Fix small issues (#9)
    * pagination supported added
    
    * fix: no refreshable on explore quiz view
    
    * fix: sweet chin music padding

--- QuizMaster/View/Main/MainView.swift
+++ QuizMaster/View/Main/MainView.swift
@@ -6,29 +6,39 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var currentTab: Tab = .Explore
+    
+    private enum Tab: String {
+        case Explore, Profile, Leaderboard
+    }
+
     var body: some View {
         // MARK: TabView with quizes and profile tab
-        TabView{
+        TabView(selection: $currentTab){
             ExploreQuizesView()
                 .tabItem{
                     Image(systemName: "rectangle.portrait.on.rectangle.portrait.angled")
                     Text("All Quiz")
-                }
+                }.tag(Tab.Explore)
             
             ProfileView()
                 .tabItem{
                     Image(systemName: "gear")
                     Text("Profile")
-                }
+                }.tag(Tab.Profile)
             
             LeaderboardView()
                 .tabItem{
                     Image(systemName: "trophy")
                     Text("Leaderboard")
-                }
+                }.tag(Tab.Leaderboard)
+            
         }.tint(Color.oPink)
     }
+    
 }
+
+
 
 #Preview {
     MainView()
